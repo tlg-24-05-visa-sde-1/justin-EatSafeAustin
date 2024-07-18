3633b9d38a628373a2121fe1fd601e095e193e86
Justin <JustinE@tlgcohort.com>
2024-07-18 17:12:14 -0500

    added datalit to input

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import SearchList from "./components/SearchList.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [allData, setAllData] = useState([]);
+  const [restaurantNames, setRestaurantNames] = useState([]);
+  //useEffect() to make API call
+  useEffect(() => {
+    let baseUrl = "https://data.austintexas.gov/resource/ecmv-9xxi.json";
+    let appToken = "u1b3qc5B97zJ4P9XMxp84hkiR";
+    let endpoint = `${baseUrl}?$$app_token=${appToken}&$limit=50000`;
+
+    fetch(endpoint, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new error(
+          "Error fetching data from City of Austin API: " + error.status
+        );
+      })
+      .then((data) => {
+        console.log(data);
+        let names = data.map((item) => {
+          return item.restaurant_name;
+        });
+        setRestaurantNames(names);
+        setAllData(data);
+        console.log(names);
+      })
+      .catch((error) => {
+        console.log("Error after fetching data: " + error);
+      });
+  }, []);
   return (
     <>
-      <SearchList></SearchList>
+      <SearchList allData={allData} names={restaurantNames}></SearchList>
     </>
   );
 }
--- src/components/SearchList.jsx
+++ src/components/SearchList.jsx
@@ -1,5 +1,32 @@
-function SearchList() {
-  return <>Here we go!</>;
+import { useState, useEffect } from "react";
+import Map from "./Map.jsx";
+
+function SearchList({ allData, names }) {
+  //state variables
+  const [clickedPlace, setClickedPlace] = useState(null);
+  const [searchTerm, setSearchTerm] = useState(null);
+
+  return (
+    <>
+      <div>
+        <form action="">
+          <input type="text" placeholder="Search" list="names" />
+          <datalist id="names">
+            {names.map((item, index) => {
+              return <option value={item} key={index}></option>;
+            })}
+          </datalist>
+        </form>
+
+        <ul></ul>
+      </div>
+      <Map
+        allData={allData}
+        setClickedPlace={setClickedPlace}
+        clickedPlace={clickedPlace}
+      ></Map>
+    </>
+  );
 }
 
 export default SearchList;
