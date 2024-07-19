2fbdea5ea75cbe2910c1787f7c547040bc305f35
Justin <JustinE@tlgcohort.com>
2024-07-19 17:11:21 -0500

    started a simple map with leaflet react

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,32 @@
 import PlaceCard from "./Card";
 import { useState, useEffect, useRef } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 function Map({ allData, setClickedPlace, clickedPlace }) {
   const mapRef = useRef(null);
   const [mapCenter, setMapCenter] = useState([30.2672, -97.7431]);
-  const [map, setMap] = useState(null);
+
+  const position = [30.2672, -97.7431];
 
   return (
     <>
-      <MapContainer></MapContainer>
+      <MapContainer
+        center={[mapCenter[0], mapCenter[1]]}
+        zoom={13}
+        ref={mapRef}
+        style={{ height: "75vh", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
       <PlaceCard></PlaceCard>
     </>
   );
